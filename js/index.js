92f0f34332193f8e2f562ae684c697abb47a6745
RickSandov <i.s.ricardo.sandoval@gmail.com>
2021-01-27 15:14:37 -0600

    Ready to use

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Initialize AOS
+AOS.init();
+
+// Get the navbar
+let navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetHeight;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  
+
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+window.onscroll = function() {myFunction()};
